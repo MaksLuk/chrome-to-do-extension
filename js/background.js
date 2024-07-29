ba9baed19489c54b13de068c60adabcba2230fe1
EternalSCA <ktototamtogda@gmail.com>
2024-07-29 22:31:30 +0300

    fix start error in background

--- js/background.js
+++ js/background.js
@@ -1,9 +1,11 @@
 (async () => {
     let data = await chrome.storage.local.get(['data']).then((result) => { return result.data; });
     let count = 0;
-    for (let page of data) {
-        for (let task of page['tasks']) {
-            if (!task.completed) count += 1;
+    if (data && data[0]) {
+        for (let page of data) {
+            for (let task of page['tasks']) {
+                if (!task.completed) count += 1;
+            }
         }
     }
     if (count == 0) count = '';
